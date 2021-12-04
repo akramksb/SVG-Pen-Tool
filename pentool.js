30f5f882463480f1ae46206ac715a5dd7f69af4f
akramksb <akram.kssiba@gmail.com>
2021-12-04 23:18:49 +0100

    fixed reverse control point + added preview

--- pentool.js
+++ pentool.js
@@ -3,6 +3,7 @@ canvas = document.querySelector(".pentoolSVG")
 
 const svgns = "http://www.w3.org/2000/svg";
 const curveStyle = "fill: none; stroke: green; stroke-width: 2px;";
+const curveStylePreview = "fill: none; stroke: #1a7d1a96; stroke-width: 1px;";
 const tangentStyle = "fill: none; stroke: rgb(212, 131, 39) ;stroke-width: 1.5px;";
 const dotStyle = "fill: rgb(235, 190, 43); stroke: none;";
 
@@ -24,6 +25,29 @@ function getCubicPath(start, ctr0, ctr1, dest) {
     return path;
 }
 
+function drawDot(canvas, className, pt, isSquare = false)
+{
+    let dot = document.querySelector(`.${className}`)
+
+    if (!dot) {
+        dot = document.createElementNS(svgns, isSquare?"rect":"circle");
+        dot.classList.add(className)
+        canvas.appendChild(dot)
+    } else
+        dot.classList.remove("invisible")
+
+    if(isSquare){
+        dot.setAttribute("x", pt[0]-3); dot.setAttribute("y", pt[1]-3);
+        dot.setAttribute("width",6); dot.setAttribute("height",6);
+    }
+    else{
+        dot.setAttribute("cx", pt[0]); dot.setAttribute("cy", pt[1]);
+        dot.setAttribute("r", 3);
+        
+    }
+    dot.setAttribute("style", dotStyle);
+}
+
 function drawFullTangent(center, pt1, pt2, canvas) {
     let tangent = document.querySelector(".fullTangent-l")
     if (!tangent) {
@@ -40,52 +64,14 @@ function drawFullTangent(center, pt1, pt2, canvas) {
     tangent.setAttribute("d", linePath)
 
     //drawing center point
-    let dot0 = document.querySelector(".fullTangent-c0")
-
-    if (!dot0) {
-        dot0 = document.createElementNS(svgns, "circle");
-        dot0.classList.add("fullTangent-c0")
-        canvas.appendChild(dot0)
-    } else
-        tangent.classList.remove("invisible")
-
-
-    dot0.setAttribute("cx", center[0]);
-    dot0.setAttribute("cy", center[1]);
-    dot0.setAttribute("r", 3);
-    dot0.setAttribute("style", dotStyle);
+    drawDot( canvas, "fullTangent-c0", center, true)
 
     //drawing dots
-
-    let dot1 = document.querySelector(".fullTangent-c1")
-    let dot2 = document.querySelector(".fullTangent-c2")
-
-    if (!dot1) {
-        dot1 = document.createElementNS(svgns, "circle");
-        dot1.classList.add("fullTangent-c1")
-        canvas.appendChild(dot1)
-    } else
-        tangent.classList.remove("invisible")
-
-    if (!dot2) {
-        dot2 = document.createElementNS(svgns, "circle");
-        dot2.classList.add("fullTangent-c2")
-        canvas.appendChild(dot2)
-    } else
-        tangent.classList.remove("invisible")
-
-    dot1.setAttribute("cx", pt1[0]);
-    dot1.setAttribute("cy", pt1[1]);
-    dot1.setAttribute("r", 3);
-    dot1.setAttribute("style", dotStyle);
-
-    dot2.setAttribute("cx", pt2[0]);
-    dot2.setAttribute("cy", pt2[1]);
-    dot2.setAttribute("r", 3);
-    dot2.setAttribute("style", dotStyle);
+    drawDot( canvas, "fullTangent-c1", pt1 );
+    drawDot( canvas, "fullTangent-c2", pt2 );
 }
 
-function drawBezierCurve( bezierCurve , save) {
+function drawBezierCurve( bezierCurve , save = false, preview = false) {
     let bezier = document.querySelector(".currentBezier")
     if (!bezier) {
         bezier = document.createElementNS(svgns, "path");
@@ -100,10 +86,42 @@ function drawBezierCurve( bezierCurve , save) {
     ${bezierCurve[2].x},${bezierCurve[2].y} \
     ${bezierCurve[3].x},${bezierCurve[3].y}`;
 
-    bezier.setAttribute("style", curveStyle);
+    bezier.setAttribute("style", preview? curveStylePreview : curveStyle);
     bezier.setAttribute("d", bezierPath)
     if (save)
         bezier.classList.remove("currentBezier")
+}
+
+function drawFirstHalfTangent( bezierCurve, canvas)
+{
+    let tangent = document.querySelector(".firstHalfTangent")
+    if (!tangent) {
+        tangent = document.createElementNS(svgns, "path");
+        tangent.classList.add("firstHalfTangent")
+        canvas.appendChild(tangent)
+    }
+    else
+        tangent.classList.remove("invisible")
+
+    let linePath = `M ${bezierCurve[0].x},${bezierCurve[0].y}\
+    L ${bezierCurve[1].x},${bezierCurve[1].y}`
+
+    tangent.setAttribute("style", tangentStyle);
+    tangent.setAttribute("d", linePath)
+
+    let dot0 = document.querySelector(".firstHalfTangent-c0")
+
+    if (!dot0) {
+        dot0 = document.createElementNS(svgns, "circle");
+        dot0.classList.add("firstHalfTangent-c0")
+        canvas.appendChild(dot0)
+    } else
+        dot0.classList.remove("invisible")
+
+    dot0.setAttribute("cx", bezierCurve[1].x);
+    dot0.setAttribute("cy", bezierCurve[1].y);
+    dot0.setAttribute("r", 3);
+    dot0.setAttribute("style", dotStyle);
 }
 
 canvas.addEventListener('mousedown', e => {
@@ -140,10 +158,9 @@ body.addEventListener('mousedown', e => {
 })
 
 canvas.addEventListener('mousemove', e => {
-    if (!mousePressed)
-        return;
 
-    if (started && !finished) {
+    // adjusting the first tangent
+    if (mousePressed && started && !finished) {
         let dx = e.offsetX - bezierCurve[0].x
         let dy = e.offsetY - bezierCurve[0].y
         // console.log( [ e.offsetX, e.offsetY ],
@@ -155,23 +172,36 @@ canvas.addEventListener('mousemove', e => {
             canvas
         )
     }
-    else if (finished) {
+    // preview of the result before adjusting the last tangent
+    else if (!mousePressed && started)
+    {
+        let ctrx = e.offsetX ;
+        let ctry = e.offsetY ;
+
+        bezierCurve2 = [...bezierCurve];
+        bezierCurve2.splice(2,0, { x:ctrx,  y:ctry }, { x:ctrx,  y:ctry })
+        drawBezierCurve( bezierCurve2, false, true)
+    }
+    // adjusting the last tangent
+    else if (mousePressed && finished) {
         let dx = e.offsetX - bezierCurve.at(-1).x
         let dy = e.offsetY - bezierCurve.at(-1).y
         // console.log( [ e.offsetX, e.offsetY ],
         //     [ bezierCurve[0].x - dx, bezierCurve[0].y - dy ] );
         let ctrx = bezierCurve.at(-1).x - dx;
         let ctry = bezierCurve.at(-1).y - dy;
+        
+        bezierCurve2 = [...bezierCurve];
+        bezierCurve2.splice(2,0, { x:ctrx,  y:ctry } )
+        drawBezierCurve( bezierCurve2 )
+
+        drawFirstHalfTangent( bezierCurve, canvas )
         drawFullTangent(
             [bezierCurve.at(-1).x, bezierCurve.at(-1).y],
             [e.offsetX, e.offsetY],
             [ctrx, ctry],
             canvas
         )
-
-        bezierCurve2 = [...bezierCurve];
-        bezierCurve2.splice(2,0, { x:ctrx,  y:ctry } )
-        drawBezierCurve( bezierCurve2 ,false)
     }
 
 })
@@ -181,12 +211,9 @@ canvas.addEventListener('mouseup', e => {
     mousePressed = false;
     // if (started && !isCtr0)
     if (started && !finished) {
-        let dx, dy;
-        dx = e.offsetX - bezierCurve[0].x
-        dy = e.offsetY - bezierCurve[0].y
         bezierCurve.push({
-            x: bezierCurve[0].x - dx,
-            y: bezierCurve[0].y - dy
+            x: e.offsetX,
+            y: e.offsetY
         })
         // isCtr0 = true
     }
